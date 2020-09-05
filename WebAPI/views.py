e838f6efe4ab2fb559d45a99aba7c342bdcf3749
liang <1229288694@qq.com>
2020-09-05 18:34:03 +0800

    前端api扩充内容

--- WebAPI/views.py
+++ WebAPI/views.py
@@ -5,10 +5,21 @@ import subprocess
 
 def API_action(request,action):
   result = {}
+  #获取real_global页面的数据，数据源来自数据库处理后的结果
+  if action == "get_real_global_data":
+    # 以下根据数据库中最近一小时内的异常记录
+    result['status'] = 'normal' #normal or abnormal
+    result['abnormal_routers'] = [1, 34, 456, 789]
+    result['abnormal_behaviors'] = ['序列号加一攻击','伪装攻击']
+
+    #以下数据库所有的异常记录
+    result['tableData'] = 
+
+
+
   if action == "get_data":
     pass #;TODO 实现获取数据的功能
-  if action == "get_abnormal_routers":
-    result['abnormal_routers']= [1,34,456]
+
 
   return JsonResponse(result, safe= False)
   # raise SuspiciousOperation("Operation not allowed.")

d5b04dd892f2dd2a79fd76b9e716906a1d803d47
liang <1229288694@qq.com>
2020-09-06 21:41:59 +0800

    real_global页面的前端通过axios获取后端数据

--- WebAPI/views.py
+++ WebAPI/views.py
@@ -11,12 +11,32 @@ def API_action(request,action):
     result['status'] = 'normal' #normal or abnormal
     result['abnormal_routers'] = [1, 34, 456, 789]
     result['abnormal_behaviors'] = ['序列号加一攻击','伪装攻击']
-
     #以下数据库所有的异常记录
-    result['tableData'] =
-
-
-
+    result['tableData'] = [{
+        'no': '1',
+        'date':'2020-8-21',
+        'time': '23:13',
+        'ID': '12',
+        'type': '序列号加一攻击',
+        'grade': '3'
+      },
+      {
+        'no': '2',
+        'date': '2020-8-21',
+        'time': '23:21',
+        'ID': '12',
+        'type': '伪装攻击',
+        'grade': '2'
+      },
+      {
+        'no': '3',
+        'date': '2020-8-21',
+        'time': '00:21',
+        'ID': '12',
+        'type': '最大序列号攻击',
+        'grade': '2'
+      },
+    ]
   if action == "get_data":
     pass #;TODO 实现获取数据的功能
 
